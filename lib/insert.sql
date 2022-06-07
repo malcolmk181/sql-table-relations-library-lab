ad71c8146240cd6a14d9958b0ca81abc9727f5d8
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-07 16:35:58 -0400

    finished sections 1 - 3

--- lib/insert.sql
+++ lib/insert.sql
@@ -31,3 +31,30 @@ VALUES
 ('Ex', 2007, 2)
 ;
 
+INSERT INTO characters (name, motto, species, author_id)
+
+VALUES
+('Jim', 'Darn', 'Human', 1),
+('Pam', 'Dang', 'Human', 1),
+('Peter', 'Oops', 'Human', 1),
+('Oni', 'Ahhh', 'Cow', 1),
+
+('Fred', 'Sumn', 'Cheddar cheese', 2),
+('Ricki', 'Oh yeah', 'Birch tree', 2),
+('Dozer', 'Squid launcher', 'Computer', 2),
+('Genesis', 'Bytes', 'Frog', 2)
+;
+
+INSERT INTO character_books(character_id, book_id)
+
+VALUES
+(1,1), (1,2), (1,3),
+(2,1), (2,2), (2,3),
+(3,1),
+(4,3),
+
+(5,4), (5,5), (5,6),
+(6,4), (6,5), (6,6),
+(7,4),
+(8,5)
+;
